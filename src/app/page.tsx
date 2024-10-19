3f1090a8145c7a727e9db8a963646f9611837e59
Allandt Bik-Elliott <allandt@gmail.com>
2024-10-19 09:32:37 +0100

    list projects on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
-export default function HomePage({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <h1>homepage test</h1>;
+import { db } from '@/db';
+import Link from 'next/link';
+
+export default async function HomePage() {
+  const data = await db.project.findMany();
+
+  return (
+    <div>
+      <h1>homepage test</h1>
+      <ul>
+        {data.map((project) => (
+          <li key={project.id}>
+            <Link href={`/projects/${project.id}`}>
+              <h3>{project.title}</h3>
+              <p>{project.summary}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
