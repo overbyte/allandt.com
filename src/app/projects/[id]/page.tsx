beb1bc891477341defe59315ba34aecb6167634f
Allandt Bik-Elliott <allandt@gmail.com>
2024-10-22 15:30:29 +0100

    add details to the individual project page and add early return with redirect to 404 page

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,12 +1,28 @@
+import { notFound } from 'next/navigation';
+import { db } from '@/db';
+
 interface ProjectPageProps {
   params: { id: string };
 }
 
-export default function ProjectPage(props: ProjectPageProps) {
-  console.log(props);
+export default async function ProjectPage(props: ProjectPageProps) {
+  const project = await db.project.findFirst({
+    where: { id: parseInt(props.params.id) },
+  });
+
+  if (!project) {
+    return notFound();
+  }
+
   return (
     <section>
-      <h1>Project</h1>
+      <h4>
+        {project.agency} | {project.client} | {project.year}
+      </h4>
+
+      <h1>{project.title}</h1>
+
+      <p>{project.description}</p>
     </section>
   );
 }
