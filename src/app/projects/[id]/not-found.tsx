d78ed939ba3471a525fa100fe06621b8f476cb22
Allandt Bik-Elliott <allandt@gmail.com>
2024-11-06 10:49:12 +0000

    add stub not found to projects

---
+++ src/app/projects/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function ProjectNotFound() {
+  return (
+    <div>
+      <h1 className="text-xl bold">
+        Sorry but that project could not be found
+      </h1>
+    </div>
+  );
+}
