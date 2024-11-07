57457e0b0223e27e9eaa1a5e87c0baaa034e40ee
Allandt Bik-Elliott <allandt@gmail.com>
2024-11-07 14:04:49 +0000

    update header component to show link to home

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className="container mx-auto px-12">
+      <Link href="/">Allandt.com</Link>
+    </header>
+  );
+}
