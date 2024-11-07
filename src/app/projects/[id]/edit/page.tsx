bca7b80bcf5cfe844a0dc160f67192485eb4320b
Allandt Bik-Elliott <allandt@gmail.com>
2024-11-07 14:30:00 +0000

    load project to edit

--- src/app/projects/[id]/edit/page.tsx
+++ src/app/projects/[id]/edit/page.tsx
@@ -1,10 +1,26 @@
+import { notFound } from 'next/navigation';
+import { db } from '@/db';
+
 interface ProjectEditPageProps {
   params: {
     id: string;
   };
 }
 
-export default function ProjectEditPage(props: ProjectEditPageProps) {
+export default async function ProjectEditPage(props: ProjectEditPageProps) {
   const id = parseInt(props.params.id);
-  return <div>editing {id}</div>;
+
+  const project = await db.project.findFirst({
+    where: { id },
+  });
+
+  if (!project) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      editing {id} {project.title}
+    </div>
+  );
 }
